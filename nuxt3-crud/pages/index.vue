1a933077a1a32a883e570a14aabd15ace7d75fd7
saryoni23 <indrasaryoni@gmail.com>
2023-05-31 21:40:51 +0700

    vue3

---
+++ nuxt3-crud/pages/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+  //meta title
+  useHead({
+    title: 'Welcome - saryoni23.github.io',
+  });
+    
+</script>
+
+<template>
+    <div class="p-5 mb-4 bg-light rounded-3">
+      <div class="container-fluid py-5">
+        <h1 class="display-5 fw-bold">NUXT 3 + LARAVEL 10</h1>
+        <p class="col-md-8 fs-4">saryoni23.github.io</p>
+      </div>
+    </div>
+</template>
